ae4c22608d785fc581dfb307cc22b7ae0e1a3097
Ryan <@>
2025-03-25 14:49:59 -0400

    Student Inserts

--- src/DatabaseInsert.java
+++ src/DatabaseInsert.java
@@ -1,5 +1,14 @@
 public class DatabaseInsert {
     public static void main(String[] args) {
+        // INSERT INTO Students ( StudentID, Name ) VALUES ( 5000 , 'Student200 ');
+        for (int i = 1; i <= 5000; i++) {
+            System.out.println("INSERT INTO Students ( StudentID, Name ) VALUES ( " + i + ", 'Student" + i + "' );");
+        }
+
+
+
+
+
 
     }
 }
