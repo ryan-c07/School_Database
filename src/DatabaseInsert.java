dcec9d812e239e4ceb59aa615775ea94fde8d9be
BT_4N2_21 <shunyic@nycstudents.net>
2025-03-28 14:11:08 -0400

    dsfffdxgfdfd

--- src/DatabaseInsert.java
+++ src/DatabaseInsert.java
@@ -6,6 +6,10 @@ import java.util.Deque;
 public class DatabaseInsert {
     public static void main(String[] args) {
         // INSERT INTO Students ( StudentID, Name ) VALUES ( 5000 , 'Student200 ');
+        ArrayList<Students> totalStudents = generateStudents();
+        ArrayList<Rooms> totalRooms = generateRooms();
+
+
         for (int i = 1; i <= 5000; i++) {
             System.out.println("INSERT INTO Students ( student_id, name ) VALUES ( " + i + ", 'Student" + i + "' );");
         }
@@ -13,9 +17,48 @@ public class DatabaseInsert {
                 "Mathematics", "Physics", "Social Studies", "Special Education", "Visual & Performing Arts", "World Languages & ENL"};
         ArrayList<Departments> departmentObjects = new ArrayList<>();
         Departments.printDepartments(departments, departmentObjects);
+        for (Students s : totalStudents){
+            System.out.println(s);
+        }
 
-
-
-
+    }
+    static ArrayList<Students> generateStudents(){
+        ArrayList<Students> students = new ArrayList<>();
+        for (int i = 1; i <= 5000; i++) {
+            students.add(new Students("Student", Integer.toString(i), (int) (Math.random() * (12 - 9 + 1) + 9), i));
+        }
+        return students;
+    }
+    static ArrayList<Rooms> generateRooms(){
+        ArrayList<Rooms> rooms = new ArrayList<>();
+        int numRooms = 1;
+        for (int f = 0; f != 9; f++){
+            switch (f) {
+                case 0:
+                    for (int r = 1; r != 21; r++){
+                        rooms.add(new Rooms(numRooms, "BN" + r));
+                        numRooms++;
+                        rooms.add(new Rooms(numRooms, "BS" + r));
+                        numRooms++;
+                        rooms.add(new Rooms(numRooms, "BE" + r));
+                        numRooms++;
+                        rooms.add(new Rooms(numRooms, "BW" + r));
+                        numRooms++;
+                    }
+                    break;
+                default:
+                    for (int r = 1; r != 21; r++){
+                        rooms.add(new Rooms(numRooms, f + "N" + r));
+                        numRooms++;
+                        rooms.add(new Rooms(numRooms, f + "S" + r));
+                        numRooms++;
+                        rooms.add(new Rooms(numRooms, f + "E" + r));
+                        numRooms++;
+                        rooms.add(new Rooms(numRooms, f + "W" + r));
+                        numRooms++;
+                    }
+            }
+        }
+        return rooms;
     }
 }
--- src/Rooms.java
+++ src/Rooms.java
@@ -1,3 +1,17 @@
 public class Rooms {
-    private int room_id, room_number;
+    private int room_id;
+    private String room_number;
+
+
+    public Rooms(int id, String room){
+        room_id = id;
+        room_number = room;
+    }
+    public int getID() {
+        return room_id;
+    }
+
+    public String toString(){
+        return "room_id: " + room_id + ", room_number: " + room_number;
+    }
 }
